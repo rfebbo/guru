edfddedb3b22107b1b96ac8d41e9fc3f7ea63fe2
Rocco Febbo <febbo87@gmail.com>
2024-06-12 14:32:21 -0400

    more robust startup

--- launch_scripts/kill_virtuosos_and_skillbridge.py
+++ launch_scripts/kill_virtuosos_and_skillbridge.py
@@ -6,7 +6,7 @@ import sys
 def main():
 
     netid = os.getenv('USER')
-    subprocess.run(f"kill $(ps aux | egrep '(virtuoso -nograph -restore)|(skillbridge/server/python_server.py)|(virtuoso -restore)' | grep {netid} | awk '{{print $2}}')", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(f"kill $(ps aux | egrep '(virtuoso -nograph -restore)|(skillbridge/server/python_server.py)|(virtuoso -restore)|(virtuoso -nographE -restore)' | grep {netid} | awk '{{print $2}}')", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     subprocess.run(f"rm -rf /tmp/skill-server-{netid}*", shell=True)
     subprocess.run(f"rm -rf /tmp/crashReport*{netid}*", shell=True) # :/
 
